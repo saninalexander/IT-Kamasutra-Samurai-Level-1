b3dede712ea05f42cbe92162de78e8de92a8aaa3
Alexander_Sanin <upmdps@gmail.com>
2021-06-13 19:19:40 +0300

    lesson_10

---
+++ src/Technologies.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+ const Technologies = () => {
+     return (
+         <div>
+             <ul>
+                 <li>CSS</li>
+                 <li>html</li>
+                 <li>js</li>
+                 <li>react</li>
+             </ul>
+         </div>
+     );
+ }
+
+ export default Technologies;
+
