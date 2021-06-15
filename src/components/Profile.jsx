79931c849cd638cb04c00c4130e4ea0de470cb40
Alexander_Sanin <upmdps@gmail.com>
2021-06-15 16:07:00 +0300

    lesson_13_end

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import classes from './Profile.module.css';
 
 const Profile = () => {
-    return  <div className='content'>
+    return  <div className={classes.content}>
         <div>
             <img src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'/  >
         </div>
@@ -12,10 +13,12 @@ const Profile = () => {
             my posts
             <div>
                 New post
-                <div>
+            </div>
+            <div className={classes.posts}>
+                <div className={classes.item}>
                     post 1
                 </div>
-                <div>
+                <div className={classes.item}>
                     post 2
                 </div>
             </div>
