95d91658c9076ec2abaf4526e2540ca533c1c2e1
Alexander_Sanin <upmdps@gmail.com>
2021-06-16 15:18:49 +0300

    lesson_17

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -7,7 +7,7 @@ const Post = (props) => {
             <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Brad_Pitt_Inglorious_Basterds_Berlin_premiere.jpg/200px-Brad_Pitt_Inglorious_Basterds_Berlin_premiere.jpg'/>
             {props.message}
             <div>
-                <span>Like</span>
+                <span>Like</span>{props.likesCount}
             </div>
         </div>
     )
