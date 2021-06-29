7c755c836bf5ff6bc7fc4bd1e70d03de255dd199
Alexander_Sanin <upmdps@gmail.com>
2021-06-29 15:19:37 +0300

    lesson_25

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -17,7 +17,7 @@ const Message = (props) => {
 
 const Dialogs = (props) => {
 
-    let dialogsData = [
+    let dialogs = [
         {id: 1, name: 'Sasha'},
         {id: 2, name: 'Masha'},
         {id: 3, name: 'Victor'},
@@ -34,16 +34,19 @@ const Dialogs = (props) => {
         {id: 5, message: 'Yo'},
     ]
 
+    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+
+    let messagesElements = messagesData.map(m => <Message message={m.message}/>);
+
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                {dialogsElements}
             </div>
 
             <div className={classes.messages}>
-                <Message message={messagesData[0].message}/>
-                <Message message={messagesData[1].message}/>
+                {messagesElements}
             </div>
         </div>
     )
